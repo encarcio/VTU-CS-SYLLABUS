e439aa6da2ae36444ea024e9de772089caea5bf9
Aditya Abhishek <encarcio95@gmail.com>
2020-06-03 12:49:45 +0530

    Added bottom Navigation and About fragment

--- app/src/main/java/com/example/vtucssyllabus/MainActivity.kt
+++ app/src/main/java/com/example/vtucssyllabus/MainActivity.kt
@@ -12,8 +12,26 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     private lateinit var navController: NavController
+    private lateinit var bottomNavigationView: BottomNavigationView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        navController = this.findNavController(R.id.myNavHostFragment)
+        bottomNavigationView = findViewById(R.id.bottom_navigation)
+        bottomNavigationView.setOnNavigationItemSelectedListener {
+            when(it.itemId)
+            {
+                R.id.navigation_about->
+                {
+                    navController.navigate(R.id.action_syllabusFragment_to_aboutFragment)
+                    true
+                }
+                R.id.navigation_syllabus->{
+                    navController.navigate(R.id.action_aboutFragment_to_syllabusFragment)
+                    true
+                }
+                else->false
+            }
+        }
     }
 }
